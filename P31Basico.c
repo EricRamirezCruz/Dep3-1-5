800c601b9b6428fbe26fb712e8c770e5aa7acf30
EricRamirezCruz <82549890+EricRamirezCruz@users.noreply.github.com>
2021-05-19 22:20:28 -0500

    Add files via upload

---
+++ P31Basico.c
@@ -0,0 +1,25 @@
+/*Dep3_prog1:Basico
+Ramirez Cruz Eric
+AMUKE*/
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    printf("\n escribe el valor de a: ");
+    scanf("%d", &a);
+    printf("\n escribe el valor de b: ");
+    scanf("%d", &b);
+    printf("\n escribe el valor de c: ");
+    scanf("%d", &c);
+
+    a = a + 1;
+    b = b * 2;
+    c = c * c;
+
+    printf ("\n\n Ahora el valor de a es %d",a);
+    printf ("\n\n Ahora el valor de b es %d",b);
+    printf ("\n\n Ahora el valor de c es %d",c);
+
+    return 0;
+}    
